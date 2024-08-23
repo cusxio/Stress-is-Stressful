03642e5aaf5b4a2e33fb4ac3226a386c931ea8d6
chen jia <aleader@Chen-Jia-2.local>
2024-08-24 01:31:46 +0800

    added moveable trash and loading screen

--- src/app/content/page.tsx
+++ src/app/content/page.tsx
@@ -15,7 +15,7 @@ export default function ContentPage() {
     <main className="bg-gradient-to-br from-dark-blue to-light-blue pl-[10%] pr-[10%] h-screen flex flex-col items-center justify-center">
       <div className="bg-white p-10 rounded-lg shadow-lg w-full max-w-2xl">
         <h1 className="text-3xl text-black font-bold mb-4">Captured Data</h1>
-        {submissions.length > 0 ? (
+        
           <ul className="text-black space-y-4">
             {submissions.map((submission, index) => (
               <li key={index} className="border-b border-gray-300 pb-2">
@@ -24,9 +24,7 @@ export default function ContentPage() {
               </li>
             ))}
           </ul>
-        ) : (
-          <p className="text-xl">No submissions yet. Be the first to share what's stressing you out!</p>
-        )}
+        
       </div>
     </main>
   );
