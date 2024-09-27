904272510a7accce066770d31912bf5404212458
chen jia <aleader@Chen-Jia-2.local>
2024-09-27 14:45:04 +0800

    change to lorem ipsum

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,13 +14,11 @@ export default function Home() {
 
       <div className="p-[10%]">
         <p className=" text-oren-4 text-xs leading-5 lg:w-[70%] m-auto">
-        Have you ever done something bad or really stupid? Like, you know, stealing money from your granny as a child or laughing at an odd-looking homeless person?
-        <br/><br/>
-        Do you still remember the day when you failed the interview for your dream job or the night when you were too shy to tell her that you loved her? Maybe you hate yourself for some of your thoughts or feelings?
-        <br/><br/>
-        It’s time to get rid of shame and start a guilt-free life! Let us save your sanity with the ancient technique of ‘Indulgence’, which first appeared in Europe more than 1,000 years ago. 
-        <br/><br/>
-        Now go scroll for redemption!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex molestiae officiis laboriosam molestias omnis adipisci minus cum iure rerum, velit, numquam consequuntur! Error, eos. Corporis magni perferendis voluptatem non minus.
+        <br></br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio fuga ipsam vero officia modi veritatis qui amet! Optio ipsam obcaecati, a ducimus esse aut temporibus blanditiis quas at vitae sequi.
+        <br></br>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum modi molestiae obcaecati incidunt quae nostrum vero quibusdam animi, enim, assumenda, magni tempora perferendis id tenetur ipsum ratione mollitia deleniti reprehenderit?
         </p>
       </div>
 
